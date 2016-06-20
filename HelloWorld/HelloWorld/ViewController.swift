ed3b6a1adce6834417538cca41fda608c894570b
David N. Cohron <dcohron@comcast.net>
2016-06-20 18:14:36 -0400

    End of class #1 20 June 2016

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by David Cohron on 6/20/16.
+//  Copyright © 2016 Trident Advisors. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
