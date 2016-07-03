b2a5b3caeaf51f52726ee97ccdaf3c2547c5672d
David N. Cohron <dcohron@comcast.net>
2016-07-03 18:44:23 -0400

    Stuck on Navigation Controller

---
+++ ArrayLogic.playground/Contents.swift
@@ -0,0 +1,145 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var past = [[1,2,3,4,5], [11,12,13,14,15], [21,22,23,24,25]]
+
+past[0]
+past[1][3]
+past[1][3] = 250
+past
+
+
+
+
+
+var present = Array(count: 10, repeatedValue: Array(count: 10, repeatedValue: false))
+//var present =[[Bool]]()
+
+
+present
+present[1][5]
+
+var x = 1
+var y = 5
+present[x][y] = true
+present
+present[1][5]
+
+print("Print Test Row \(x), Column \(y)")
+
+
+for row in 0...9 {
+    for column in 0...9 {
+        if arc4random_uniform(3) == 1 {
+            present[row][column] = true
+        }
+        else {
+            present[row][column] = false
+        }
+    }
+}
+
+present
+
+
+// instantiate array of boolean arrays for both before and after
+var before = Array(count: 10, repeatedValue: Array(count: 10, repeatedValue: false))
+var after = Array(count: 10, repeatedValue: Array(count: 10, repeatedValue: false))
+
+
+// initialize array
+for row in 0...9 {
+    for column in 0...9 {
+        if arc4random_uniform(3) == 1 {
+            before[row][column] = true
+        }
+        else {
+            before[row][column] = false
+        }
+    }
+}
+
+before
+
+
+//  Function to count lives in total array
+func totalCount(inputArray:[[Bool]]) -> Int {
+    var lifeCount = 0
+    for row in 0...9 {
+        for column in 0...9 {
+            if inputArray[row][column] == true {
+                lifeCount += 1
+            }
+        }
+    }
+    return lifeCount
+}
+
+totalCount(before)
+totalCount(after)
+
+var row = 8
+var column = 9
+var count = 0
+
+before[(row+1)%10][(column-1)%10]
+before[(row+1)%10][(column)%10]
+before[(row+1)%10][(column+1)%10]
+before[(row)%10][(column-1)%10]
+before[(row)%10][(column+1)%10]
+before[(row-1)%10][(column-1)%10]
+before[(row-1)%10][(column)%10]
+before[(row-1)%10][(column+1)%10]
+
+if before[(row+1)%10][(column-1)%10] {count += 1}
+if before[(row+1)%10][(column)%10] {count += 1}
+if before[(row+1)%10][(column+1)%10] {count += 1}
+if before[(row)%10][(column-1)%10] {count += 1}
+if before[(row)%10][(column+1)%10] {count += 1}
+if before[(row-1)%10][(column-1)%10] {count += 1}
+if before[(row-1)%10][(column)%10] {count += 1}
+if before[(row-1)%10][(column+1)%10] {count += 1}
+
+count
+
+
+
+//func neighborCount(inputArray:[[Bool]], row:Int, column:Int) -> Int {
+//    var aliveCount = 0
+//    if inputArray[row+1][column-1] { aliveCount +=1 }
+//    if inputArray[row+1][column] { aliveCount +=1 }
+//    if inputArray[row+1][column+1] { aliveCount +=1 }
+//    if inputArray[row][column-1] { aliveCount +=1 }
+//    if inputArray[row][column+1] { aliveCount +=1 }
+//    if inputArray[row-1][column-1] { aliveCount +=1 }
+//    if inputArray[row-1][column] { aliveCount +=1 }
+//    if inputArray[row-1][column+1] { aliveCount +=1 }
+//    return aliveCount
+//}
+//
+//neighborCount(before, row:2, column:3)
+
+
+//
+//
+//var currentCell = false
+//
+//Switch currentCell {
+//    case true:
+//        if neighborCount(row, column:column) == 2, 3 {
+//            after[row][column] = true
+//        }
+//        else {
+//            after[row][column] = false
+//        }
+//    case false:
+//        if neigborCount(row, column:column) == 3 {
+//            after[row][column] = true
+//        }
+//        else {
+//            after[row][column] = false
+//        }
+//
+//    }
+
--- Assignment2/Assignment2/MainViewController.swift
+++ Assignment2/Assignment2/MainViewController.swift
@@ -23,3 +23,5 @@ class MainViewController: UIViewController {
 
 }
 
+
+
